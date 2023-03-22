44373c3e1e5696d526bea47fbc78dd5669fd3e20
Haani <116673121+haanisyed@users.noreply.github.com>
2023-03-22 16:15:53 -0400

    Update a3_q1.py

--- a3_q1.py
+++ a3_q1.py
@@ -1,10 +1,7 @@
 '''
-CISC-121 2023W
+Python 121 Assignment 3 Q1
 Name: Haani Syed
-Student Number: 20331181
-Email: 21ahs7@queensu.ca
 Date: 2023-03-08
-I confirm that this assignment solution is my own work and conforms to Queen’s standards of Academic Integrity.
 '''
 import csv #imports csv file with data set
 from functions3 import conversion, menu, displayInfo, uniqueDistricts, totalVotes, findMax, findMin, totalVotes, selectionSort, bubbleSort, binarySearch
